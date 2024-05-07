d7ad1bf51c6be43aab76df7521fc6a32d8ed5bd3
karim-daw <karimdaw91@gmail.com>
2024-05-07 22:46:51 +0100

    Testing github card component in app

--- components/src/partials/projects.tsx
+++ components/src/partials/projects.tsx
@@ -1,6 +1,7 @@
 import { getPosts } from "../utils";
 
 import { Card } from "../card";
+import GitHubRepoCard from "../githubCard";
 
 export function ProjectsPartial() {
   let projects = getPosts("projects");
@@ -15,19 +16,28 @@ export function ProjectsPartial() {
   });
   // .sort((a, b) => (a.start < b.start ? 1 : -1));
 
+  const repoDetails = {
+    name: "speckle-server",
+    description: "The Speckle Server",
+    stargazers_count: 100,
+    forks_count: 50,
+    language: "TypeScript",
+  };
+
   return (
     <div>
       <div className="py-10">
         {!parsedProjects.length
           ? "No published projects"
           : parsedProjects.map((e) => (
-              <Card
-                key={e.slug}
-                title={e.title}
-                slug={e.slug}
-                color="gray-500"
-                type="project"
-              />
+              // <Card
+              //   key={e.slug}
+              //   title={e.title}
+              //   slug={e.slug}
+              //   color="gray-500"
+              //   type="project"
+              // />
+              <GitHubRepoCard repoDetails={repoDetails} />
             ))}
       </div>
     </div>
